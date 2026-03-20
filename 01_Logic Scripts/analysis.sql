3cec4de099ca904c5a7efd3d98a55e8431a379ee
Daniel Cheng <dcheng6775@gmail.com>
2026-03-19 23:48:04 -0400

    minor edit

--- 01_Logic Scripts/analysis.sql
+++ 01_Logic Scripts/analysis.sql
@@ -1,6 +1,6 @@
 -- Used to perform data analysis for Tableau
 IF [NDVI_Score] < 0.2 AND [Rain_Last_24h] > 40 THEN "Predicted Drain Damage"
-ELSEIF [Rain_Last_24h] > 35 THEN "Waterlogged"
+ELSEIF [Rain_Last_24h] > 30 AND [NDVI_Score] < 0.4 THEN "Waterlogged"
 ELSEIF [Surface_Temp] > 31 AND [NDVI_Score] < 0.4 THEN "Crop Stress"
 ELSEIF [Rain_Last_24h] > 20 THEN "Potential Drain Risk"
 ELSE "Nominal"
